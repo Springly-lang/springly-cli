7d5736e16a87c5909814f6ce02a9712cffceec87
jalalx <sjamini@gmail.com>
2020-07-11 02:12:41 +0430

    navigate to url fixed.

--- src/Springly/Commands/NavigateBrowserCommand.cs
+++ src/Springly/Commands/NavigateBrowserCommand.cs
@@ -1,17 +1,22 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Springly.Commands
 {
     public class NavigateBrowserCommand : RegexCommandBase
     {
         protected override string[] Patterns => new[] {
-            @"^(navigate)\s+to\s+(?<url>\w+:\/\/[\w@][\w.:@]+\/?[\w\.?=%&=\-+@/$,]*)",
-            @"^(navigate)\s+to\s+(?<url>\w+:\/\/\/[\w@][\w.:@]+\/?[\w\.?=%&=\-+/$,]*)"
+            @"^(navigate)\s+to\s+(?<url>.+)"
         };
 
         protected override ExecutionResult InternalExecute(Match match, int patternIndex, ExecutionContext context)
         {
             var url = match.Groups["url"].Value;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out _))
+            {
+                throw new SyntaxErrorException($"Url '{url}' is not valid.");
+            }
+
             context.Current.Navigate().GoToUrl(url);
             return ExecutionResult.Success;
         }
